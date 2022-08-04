75d6e922675df8d99ecbfe9c27d5252451af97b6
Alexis Bonnet <a.bonnet@codeur.online>
2022-08-04 09:04:46 +0200

    Empêchement de l'exécution de l'animation au chargement de la page.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*{
+ *    titre: "", 
+ *    description: "",
+ *    ingredients: ["…", "…", "…"],
+ *    etapes: ["…", "…", "…"],
+ *    indicateurs: {
+ *        difficulte: x,
+ *        duree: x,
+ *        prix: x
+ *    }
+ *
+ *}
+*/
+
 var html = document.querySelector('html');
 var liste_recettes = document.getElementById('liste-recettes');
 var consultation_recette = document.getElementById('consultation-recette');
@@ -6,6 +20,9 @@ var consultation_recette = document.getElementById('consultation-recette');
  * Fait défiler les sections "liste_recettes" et "consultation_recette" alternativement de droite à gauche et de gauche à droite.
  */
 function defiler(){
+    liste_recettes.style.animationDuration = '1500ms';
+    consultation_recette.style.animationDuration = '1500ms'
+
     let decalage = html.scrollTop;
     html.scrollTop = 0;
     if (liste_recettes.classList.contains('visible')) {
